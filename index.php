0ec6bee0bb3688189279957c39293f6fc3296e4a
hashmod <hashmod190@Gmail.com>
2017-03-18 04:20:20 +0000

    Create index.php
    Script html in page php initial push

---
+++ index.php
@@ -0,0 +1,16 @@
+<!Doctype hyml>
+<html>
+<head>
+<title>Page Menu</title>
+<meta charset = UTF-8>
+</head>
+<body>
+     <h1>Menu</h1>
+     <ul>
+        <li>Aceuil</li>
+        <li>Cours</li>
+        <li>Cours</li>
+        <li>Contact</li>
+    </ul>    
+</body>
+</html>
